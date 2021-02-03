1cc2e64d06e491c471ee0abc5a626522111db609
Sakharam Shinde <shinde_sk@India.XoriantCorp.com>
2021-02-03 14:01:00 +0530

    g

--- index.php
+++ index.php
@@ -2,14 +2,12 @@
 
 echo ("hello world!");
 
-/* SQL Server Extension Sample Code:
+// SQL Server Extension Sample Code:
 $connectionInfo = array("UID" => "sqladmin", "pwd" => "P@ssword1!", "Database" => "db1", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:ogmssqlserver.database.windows.net,1433";
 
 $conn = sqlsrv_connect($serverName, $connectionInfo);
-for($k=1; $k<=50; $k++){
 sqlsrv_connect($serverName, $connectionInfo);
-
 
 
 $sql = "select * from [SalesLT].[vGetAllCategories]";
@@ -20,9 +18,6 @@ if( $stmt === false) {
 
 while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
       echo ($row['ProductCategoryName']);
-      echo("<br/>")
+      echo("<br/>");
 }
-
 sqlsrv_free_stmt( $stmt);
-}
-*/
